3c98167812d2e1e11a9b4ec3d4f2618c4ef552ce
usrrname <usrrname@users.noreply.github.com>
2019-01-07 17:21:45 -0500

    menu add responsive class fix

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,5 @@
 //      shamrock button toggle colour background
-let button = document.getElementById('bgButton');
+const button = document.getElementById('bgButton');
 const bg = document.getElementById('moving-bg');
 button.addEventListener('click', (e) => {
         if (bg.style.display !== 'block' || bg.style.display == 'none'){
@@ -16,17 +16,17 @@ button.addEventListener('click', (e) => {
 
 // toggle between responsive menu in smaller screens
 const listItems= document.getElementById('menu').children;
-
+console.log(listItems);
 const noBootstrap = () => {
         const nav = listItems;
                 for (var i = 0; i < nav.length; i++) {
                 if (!nav[i].classList.contains('responsive')){           
-                        nav[i].className = "responsive";
-                      nav[i].firstChild.style.display = "inline-block";
+                        nav[i].className.add = "responsive"
+                      nav[i].firstChild.style.display = "block"
                 }
-                if (nav[i].classList.contains('responsive')){
+                        if (nav[i].className=='responsive'){
                         nav[i].classList.remove('responsive')
-                nav[i].style.display = "none";
+                        nav[i].style.display = "none";
         }
         }       
 }
@@ -41,7 +41,7 @@ const anchors = [];
 for (let i = 0; i < listItems.length; i++) { 
        const anchor = listItems[i].firstChild.attributes[0].nodeValue;
         link_array.push(anchor);
-        listItems[i].addEventListener('click', function (e) {
+        listItems[i].addEventListener('click', (e) => {
         //console.log(this.firstChild.attributes[0].nodeValue + ' clicked')
         e.preventDefault();
         document.querySelector(this.firstChild.attributes[0].nodeValue).scrollIntoView({
